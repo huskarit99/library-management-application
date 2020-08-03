da6ac6dbde1971c32a197ce2c0bb79b3ec5d6dc9
minhquoc5599 <minhquoc5599@gmail.com>
2020-08-03 08:24:02 +0700

    init project

---
+++ LibraryManagement/app/src/main/java/com/example/librarymanagement/networks/CheckConnect.java
@@ -0,0 +1,25 @@
+package com.example.librarymanagement.networks;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
+import android.widget.Toast;
+
+public class CheckConnect {
+    public boolean isconnected(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if(connectivityManager==null){
+            return false;
+        }
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            Network network = connectivityManager.getActiveNetwork();
+            NetworkCapabilities capabilities = connectivityManager.getNetworkCapabilities(network);
+            return capabilities != null && (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) || capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR));
+        }
+        return true;
+    }
+    public static void ShowToast_Short(Context context,String information){
+        Toast.makeText(context,information, Toast.LENGTH_SHORT).show();
+    }
+}

83c23d1e8b29fb542dbe16ada1ff87578d69c7da
huskarit99 <nguyenthaihocxm1999@gmail.com>
2020-08-05 13:35:12 +0700

    ADD Upload by choosing photo from gallery or taking photo| MOD Calender

--- LibraryManagement/app/src/main/java/com/example/librarymanagement/MainActivity.java
+++ LibraryManagement/app/src/main/java/com/example/librarymanagement/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.librarymanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import android.os.Bundle;
+import android.widget.Toast;
+import android.content.pm.PackageManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+//        ActivityCompat.requestPermissions(MainActivity.this,
+//                new String[]{AndroidManifest.permission.READ_EXTERNAL_STORAGE},
+//                1);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_information_book);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(MainActivity.this, "Permission denied to read your External storage", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 }
--- LibraryManagement/app/src/main/java/com/example/librarymanagement/activities/AddUserActivity.java
+++ LibraryManagement/app/src/main/java/com/example/librarymanagement/activities/AddUserActivity.java
@@ -1,22 +1,24 @@
 package com.example.librarymanagement.activities;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.app.DownloadManager;
+import android.app.DatePickerDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.CompoundButton;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -27,17 +29,37 @@ import com.example.librarymanagement.R;
 import com.example.librarymanagement.networks.CheckConnect;
 import com.example.librarymanagement.networks.Server;
 
-import java.io.Console;
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
+import java.io.FileNotFoundException;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
+import android.content.Intent;
+import android.net.Uri;
+import android.database.Cursor;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.os.Environment;
+import java.io.File;
+import java.io.OutputStream;
+
 
 public class AddUserActivity extends AppCompatActivity {
 
-    Toolbar toolbar;
-    EditText edtName, edtIdUser, edtBirthDay, edtEmail, edtAddress;
-    String user_id, name, role, gender, email, birthday, address, password, image;
-    RadioButton radioButton;
-    RadioGroup radioGroup;
+    private Toolbar toolbar;
+    private EditText edtName, edtIdUser, edtEmail, edtBirthday, edtAddress;
+    private ImageView edtImage, btnCalendar;
+    private String user_id, name, role, gender, email, birthday, address, password;
+    private String image;
+    private RadioButton radioButton;
+    private RadioGroup radioGroup;
+    private DatePickerDialog datePickerDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,15 +71,36 @@ public class AddUserActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("Thêm độc giả");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        edtImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectImage();
+            }
+        });
+
         int radioId = radioGroup.getCheckedRadioButtonId();
         radioButton = findViewById(radioId);
         gender = radioButton.getText().toString();
 
-
+        btnCalendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar calendar = Calendar.getInstance();
+                final int year = calendar.get(Calendar.YEAR);
+                final int month = calendar.get(Calendar.MONTH);
+                final int day = calendar.get(Calendar.DAY_OF_MONTH);
+                datePickerDialog = new DatePickerDialog(AddUserActivity.this, new DatePickerDialog.OnDateSetListener(){
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int day) {
+                        month = month + 1;
+                        String date = year+"-"+month+"-"+day;
+                        edtBirthday.setText(date);
+                    }
+                }, year, month, day);
+                datePickerDialog.show();
+            }
+        });
     }
-
-
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -73,10 +116,10 @@ public class AddUserActivity extends AppCompatActivity {
                 role ="1";
                 name = edtName.getText().toString();
                 email = edtEmail.getText().toString();
-                birthday = edtBirthDay.getText().toString();
+                birthday = edtBirthday.getText().toString();
                 address = edtAddress.getText().toString();
                 password = "123456";
-                image = "123";
+                System.out.println(image.length());
                 RequestQueue requestQueue = Volley.newRequestQueue(getApplication());
                 StringRequest stringRequest = new StringRequest(Request.Method.POST, Server.addUser,
                         new Response.Listener<String>() {
@@ -109,7 +152,6 @@ public class AddUserActivity extends AppCompatActivity {
                     }
                 };
                 requestQueue.add(stringRequest);
-
             }
         }else{
             Toast.makeText(AddUserActivity.this, "Lỗi mạng", Toast.LENGTH_SHORT).show();
@@ -118,13 +160,76 @@ public class AddUserActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == 1) {
+                Bundle extras = data.getExtras();
+                Bitmap imageBitmap = (Bitmap) extras.get("data");
+                edtImage.setImageBitmap(imageBitmap);
+            } else if (requestCode == 2) {
+                Uri selectedImage = data.getData();
+                String[] filePath = {MediaStore.Images.Media.DATA};
+                Cursor c = getContentResolver().query(selectedImage, filePath, null, null, null);
+                c.moveToFirst();
+                int columnIndex = c.getColumnIndex(filePath[0]);
+                String picturePath = c.getString(columnIndex);
+                c.close();
+                Bitmap thumbnail = (BitmapFactory.decodeFile(picturePath));
+                Log.w("path of image from gallery......******************.........", picturePath + "");
+                edtImage.setImageBitmap(thumbnail);
+                BitMapToString(thumbnail);
+            }
+        }
+    }
+
+    public String BitMapToString(Bitmap userImage) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        userImage.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] imageBytes = baos.toByteArray();
+        image = Base64.encodeToString(imageBytes, Base64.DEFAULT);
+        return image;
+    }
+
+    private void selectImage(){
+        AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+        builder1.setMessage("Write your message here.");
+        builder1.setCancelable(true);
+
+        builder1.setPositiveButton(
+                "Take Photo",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                        startActivityForResult(intent, 1);
+                    }
+                });
+
+        builder1.setNegativeButton(
+                "Choose from Gallery",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Intent intent = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                        startActivityForResult(intent, 2);
+                    }
+                });
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
+    }
+
     private void mapping() {
         radioGroup = findViewById(R.id.radioGroup);
         toolbar = findViewById(R.id.toolbarAddUser);
+        edtBirthday = findViewById(R.id.edtBirthday);
         edtName = findViewById(R.id.edtName);
         edtIdUser = findViewById(R.id.edtIdUser);
-        edtBirthDay = findViewById(R.id.edtBirthday);
         edtEmail = findViewById(R.id.edtEmail);
         edtAddress = findViewById(R.id.edtAddress);
+        edtImage = findViewById(R.id.avatarUser);
+        btnCalendar = findViewById(R.id.btnCalendar);
     }
 }
